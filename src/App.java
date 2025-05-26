83deea4aeebda31c5da893862d276b3a57ee30a5
tsxmx <57422526+tsxmx@users.noreply.github.com>
2025-05-26 21:10:08 +0200

    ver 1.1.6 // 26/05/2025
    REPOSITORY
    - READ del Evento hecho

--- src/App.java
+++ src/App.java
@@ -7,21 +7,6 @@ import java.util.List;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class App {
-    public static void main(String[] args) {
 
 
-
-        PartidoRepository PR = new PartidoRepository();
-        Partido partido;
-
-        partido = PR.findById(2);
-
-
-
-            System.out.println(partido.getFechaInicio());
-
-
-
-
-    }
 }
--- src/Repository/EventoRepository.java
+++ src/Repository/EventoRepository.java
@@ -1,13 +1,24 @@
 package Repository;
 
 import Entity.Evento;
+import Entity.Jugador;
+import Entity.Partido;
+import Entity.TipoEvento;
 import Util.DBC;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class EventoRepository implements Repository<Evento>{
+
+    JugadorRepository JR = new JugadorRepository();
+    PartidoRepository PR = new PartidoRepository();
+    TipoEventoRepository TR = new TipoEventoRepository();
 
 
     private Connection getConnection() throws SQLException
@@ -20,7 +31,44 @@ public class EventoRepository implements Repository<Evento>{
      */
     @Override
     public List<Evento> findAll() {
-        return List.of();
+
+        List<Evento> eventos = new ArrayList<>();
+        try
+        {
+            Connection con = getConnection();
+
+            String query = "SELECT idEvento, " +
+                                    "minuto, " +
+                                    "jugador_idJugador, " +
+                                    "Partido_id, " +
+                                    "TipoEvento_idTipoEvento " +
+                            "FROM evento";
+
+            PreparedStatement pstmt = con.prepareStatement(query);
+            ResultSet rs = pstmt.executeQuery();
+
+            while(rs.next())
+            {
+                int idEvent = rs.getInt("idEvento");
+                int minuto = rs.getInt("minuto");
+                Jugador jug = JR.findById(rs.getInt("jugador_idJugador"));
+                Partido par =PR.findById(rs.getInt("partido_id"));
+                TipoEvento TipEv =  TR.findById(rs.getInt("TipoEvento_idTipoEvento"));
+
+                Evento event = new Evento(idEvent, minuto, jug, par, TipEv);
+
+                jug.addEvento(event);
+                par.addEvento(event);
+                TipEv.addEvento(event);
+
+                eventos.add(event);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return eventos;
+
     }
 
     /**
@@ -29,7 +77,9 @@ public class EventoRepository implements Repository<Evento>{
      */
     @Override
     public Evento findById(int id) {
+
         return null;
+
     }
 
     /**
